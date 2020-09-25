ff8603b4e033f6aecb94af46672a3c1836053a0f
Sarvesh Chezhian <savvychez@gmail.com>
2020-09-24 20:46:57 -0500

    added lines

--- lines/canvas.js
+++ lines/canvas.js
@@ -1,8 +1,8 @@
-const fps = 1
+const fps = 40
 const gridSquareSize = 1
 const initSnakeCount = 100
-const snakeLen = 10
-const spacingCoefficient = 100
+const snakeLen = 15
+const spacingCoefficient = 200
 
 let lineW;
 let lineH;
@@ -16,9 +16,12 @@ var ctx
 
 window.onload = e => {
     canvas = document.getElementById('canvas')
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
+    canvas.width = window.innerWidth*2
+    canvas.height = window.innerHeight*2
     ctx = canvas.getContext('2d')
+
+    // ctx.transform(1, 1, 1, 1, 1, 1);
+    // ctx.rotate(5 * Math.PI / 180);
 
     build()
 }
@@ -38,7 +41,6 @@ const debounce = (func, wait) => {
 }
 
 const resizePostBounce = debounce(() => {
-    console.log("debounced");
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
     build()
@@ -58,21 +60,15 @@ const update = () => {
         for (var x = 0; x < lineW; x++) {
             if (lines[y][x] == snakeLen) {
                 choices = []
-                if (checkPos(y - 1, x) === 0 && (checkPos(y - 2, x) == 0))
-                    choices.push([y - 1, x])
-                if (checkPos(y + 1, x) === 0 && (checkPos(y + 2, x) == 0))
-                    choices.push([y + 1, x])
+                // if (checkPos(y - 1, x) === 0 && (checkPos(y - 2, x) == 0))
+                    // choices.push([y - 1, x])
+                // if (checkPos(y + 1, x) === 0 && (checkPos(y + 2, x) == 0))
+                    // choices.push([y + 1, x])
                 if (checkPos(y, x + 1) === 0 && (checkPos(y, x + 2) == 0)) {
                     choices.push([y, x + 1])
-                    choices.push([y, x + 1])
-                    choices.push([y, x + 1])
-
                 }
                 if (checkPos(y, x - 1) === 0 && (checkPos(y, x - 2) == 0 )) {
                     choices.push([y, x - 1])
-                    choices.push([y, x - 1])
-                    choices.push([y, x - 1])
-
                 }
 
                 if (choices.length > 0) {
@@ -103,45 +99,31 @@ const render = () => {
     var yLen = lines.length
     var xLen = lines[0].length
 
-    ctx.fillStyle = "#FFFFFF"
+    ctx.fillStyle = "#05041a"
     ctx.clearRect(0, 0, wCan, hCan)
     ctx.fillRect(0, 0, wCan, hCan)
 
     for (var y = 0; y < yLen; y++) {
         for (var x = 0; x < xLen; x++) {
-            // document.write(lines[y][x] + " ")
-            if (lines[y][x] == 5) {
-                ctx.fillStyle = "#FF00FF"
-            } else if (lines[y][x] == 4) {
-                ctx.fillStyle = "#00FFFF"
-            } else if (lines[y][x] == 3) {
-                ctx.fillStyle = "#0000FF"
-            } else if (lines[y][x] == 2) {
-                ctx.fillStyle = "#00FF00"
-            } else {
-                ctx.fillStyle = "#000000"
-            }
             if (lines[y][x] > 0) {
-                // let col = 1 * lines[y][x];
-
-                ctx.fillStyle = "rgb(" + col + "," + col + "," + 10 * col + ")"
+                let col = 10 * lines[y][x];
+                ctx.fillStyle = "rgb(" + col + "," + col + "," + 3 * col + ")"
             } else {
                 ctx.fillStyle = "#000000"
             }
-
 
             var xPos = Math.round(x * ((wCan + Math.floor(wCan / xLen) - gridSquareSize) / (xLen)))
             var yPos = Math.round(y * ((hCan + Math.floor(hCan / yLen) - gridSquareSize) / (yLen)))
 
             ctx.fillRect(xPos, yPos, gridSquareSize, gridSquareSize)
 
-            if (lines[y][x] > 0 && lines[y + 1] && lines[y + 1][x] > 0 && ((lines[y + 1][x] == lines[y][x] - 1) || (lines[y + 1][x] == lines[y][x] + 1))) {
-                ctx.fillRect(xPos, yPos, gridSquareSize, Math.ceil(hCan / yLen))
-            }
+            // if (lines[y][x] > 0 && lines[y + 1] && lines[y + 1][x] > 0 && ((lines[y + 1][x] == lines[y][x] - 1) || (lines[y + 1][x] == lines[y][x] + 1))) {
+            //     // ctx.fillRect(xPos, yPos, gridSquareSize, Math.ceil(hCan / yLen))
+            // }
             if (lines[y][x] > 0 && lines[y][x + 1] > 0) {
-                if(lines[y][x] == snakeLen && lines[y][x] == lines[y][x+2]) {
-                    ctx.fillStyle = "#FFFFFF"
-                    ctx.fillRect(xPos, yPos, Math.ceil(wCan / xLen), gridSquareSize)
+                if(lines[y][x+1] == snakeLen && lines[y][x+4] == snakeLen) {
+                    // ctx.fillStyle = "#09072e"
+                    // ctx.fillRect(xPos, yPos, Math.ceil(wCan / xLen), gridSquareSize)
                 } else {
                     ctx.fillRect(xPos, yPos, Math.ceil(wCan / xLen), gridSquareSize)
                 }
@@ -162,19 +144,15 @@ const rand = (min, max) => {
 
 const recursiveSnake = () => {
     createSnake()
-    snakeTimeout = setTimeout(recursiveSnake, (1000 / (60)))
+    snakeTimeout = setTimeout(recursiveSnake, (1000 / (fps)))
 }
 
 const createSnake = () => {
     let randVar = rand(1, 3)
     if (randVar == 1)
         lines[rand(0, lineH)][0] = snakeLen
-    else if (randVar == 2)
+    else
         lines[rand(0, lineH)][lineW - 1] = snakeLen
-    else if (randVar == 3)
-        lines[0][rand(0, lineW)] = snakeLen
-    else if (randVar == 4)
-        lines[lineH - 1][rand(0, lineW)] = snakeLen
 }
 
 const build = () => {
@@ -184,9 +162,7 @@ const build = () => {
     lineW = ~~((window.innerHeight / window.innerHeight) * spacingCoefficient)
     lineH = ~~((window.innerHeight / window.innerWidth) * spacingCoefficient)
 
-
     lines = new Array(lineH)
-
     for (var y = 0; y < lineH; y++) {
         lines[y] = new Array(lineW)
     }
@@ -196,11 +172,6 @@ const build = () => {
         }
     }
 
-    for (var i = 0; i < initSnakeCount; i++) {
-        createSnake()
-    }
     recursiveSnake()
-
-    createSnake()
     render()
 }
